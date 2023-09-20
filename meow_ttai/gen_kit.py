0dd32e104a6a1f61c7ca49e77254b1dd93346c9d
f1shy-dev <56125930+f1shy-dev@users.noreply.github.com>
2023-09-20 22:18:58 +0100

    meow :3

--- meow_ttai/gen_kit.py
+++ meow_ttai/gen_kit.py
@@ -1,9 +1,12 @@
+from IPython.display import display, Markdown
+import openai
 import os
 import subprocess
 import yt_dlp
 import whisper
 import ffmpeg
 from .utils import write_srt, write_compact_srt
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 #####################
 #     Variables     #
@@ -13,13 +16,13 @@ MODEL = os.environ.get("WHISPER_MODEL", "small.en") # or "medium.en" or "large.e
 
 #####################
 
-def main():
+def download_and_transcribe(url=None):
     # Ensure that the necessary directories exist
     os.makedirs("./workspace", exist_ok=True)
 
     # Ask for a YouTube URL
     # url = input("meow <3! enter your youtewb url: ")
-    url = os.environ.get("WHISPER_URL", input("meow <3! enter your youtewb url: "))
+    url = url or input("meow <3! enter your youtewb url: ")
     # url = "https://www.youtube.com/watch?v=xFWakbQAk5Q"
 
     print("Downloading video info...")
@@ -81,6 +84,41 @@ def main():
 
     with open(f"./workspace/{video_id}/gen_final/subtitles.txt", "w", encoding="utf-8") as txt:
         txt.write(result["text"])
+    
+    return video_id
+
+
+def analyse_with_chatgpt(video_id):
+    system_prompt = """
+You are an analyser, which looks at the transcripts of podcasts and selects a few (max three) high quality 10-30second (must be AT LEAST 10 seconds, dont pick out like 15 short segments, only a few high quality ones) segments which would engage the most largest audience and most amount of viewers in a video clip on social media - audience would be age of between 13 and 24, and dont forget to consider things such as the main hooks in the transcript, and stuff like controversial opinions or interesting facts which could attract attention
+
+You are to return a JSON array of objects, where in the object: "start" and "end" are the start/end points, "summary" is a one sentence (max 30word) summary of the clip section, and "reason" is a reason from you about why you picked this clip (max 15 words)
+
+example:
+[{
+  "start":"0:00:28.300",
+  "end": "0:00:45.620",
+  "summary": "Freedom won't happen for the rest of the world, the divide will remain.",
+  "reason": "Super controversial opinion - would engage audience/comments"
+}]"""
+    file = open(f"./workspace/{video_id}/gen_final/subtitles.csrt", "r", encoding="utf-8")
+    transcript = file.read()
+    print("Analyzing transcript with ChatGPT...")
+    response = openai.ChatCompletion.create(
+              model="gpt-3.5-turbo-16k",
+              messages=[{"role": "system", "content": system_prompt},
+                        {"role": "user", "content": file.read()}
+              ])
+
+    usage = response["usage"]
+    prompt = usage['prompt_tokens']
+    comp = usage['completion_tokens']
+    print(f"Used {prompt} prompt + {comp} completion ({usage['total_tokens']} total ~ ${(prompt/1000*0.0015) + (comp/1000*0.002)}) tokens.")
+    print(response["choices"][0]["message"]["content"])
+
+    with open(f"./workspace/{video_id}/gen_final/analysis.json", "w", encoding="utf-8") as json:
+        json.write(response["choices"][0]["message"]["content"])
 
 if __name__ == "__main__":
-    main()
+    video_id = download_and_transcribe()
+    analyse_with_chatgpt(video_id)
