23299de9e650b28eb9761033236ff4e1ad7681c9
f1shy-dev <56125930+f1shy-dev@users.noreply.github.com>
2023-10-08 23:16:12 +0100

    introducing subs! flawed

--- ttai_farm/farm/clipper.py
+++ ttai_farm/farm/clipper.py
@@ -33,12 +33,6 @@ def clip_video(workspace_dir: str, skip_clip_if_cached: bool, video_info: VideoI
         for file_name in os.listdir(os.path.join(video_folder, "clips")):
             os.remove(os.path.join(video_folder, "clips", file_name))
 
-    if os.path.exists(os.path.join(video_folder, "clips")) and len(os.listdir(os.path.join(video_folder, "clips"))) > 0:
-        console.log(
-            f"Found {len(os.listdir(os.path.join(video_folder, 'clips')))} cached clips for video '{video_info.extractor}-{video_info.video_id}'")
-        if len(os.listdir(os.path.join(video_folder, "clips"))) == len(json.load(open(analysis_path, "r", encoding="utf-8"))):
-            return
-
     try:
         with open(analysis_path, "r", encoding="utf-8") as afile:
             analysis = json.load(afile)
@@ -68,9 +62,57 @@ def clip_video(workspace_dir: str, skip_clip_if_cached: bool, video_info: VideoI
             if os.path.exists(clip_path):
                 continue
 
-            command = f"ffmpeg -y -i \"{video_path}\" -ss {chunk.start} -to {chunk.end} -c copy \"{clip_path}\""
-            output = subprocess.run(command, shell=True, capture_output=True)
+            # command = f"ffmpeg -y -i \"{video_path}\" -ss {chunk.start} -to {chunk.end} -c copy \"{clip_path}\" -vf \"crop=ih*(9/16):ih\""
+            command = [
+                "ffmpeg", "-y", "-i", video_path,
+                "-ss", chunk.start,
+                "-to", chunk.end,
+                "-vf", "crop=ih*(9/16):ih",
+                "-c:a", "copy",
+                clip_path,
+            ]
+            output = subprocess.run(command,  capture_output=True)
             assert output.returncode == 0, f"Failed to clip video: {output.stderr.decode('utf-8')}"
+            progress.update(main_bar, advance=1)
+
+    clip_folder = os.path.join(
+        workspace_dir, "clips", video_info.folder_name())
+    os.makedirs(clip_folder, exist_ok=True)
+
+    with Progress(
+        SpinnerColumn(),
+        TextColumn("{task.description}"),
+        BarColumn(),
+        MofNCompleteColumn(),
+        transient=True,
+    ) as progress:
+        main_bar = progress.add_task(
+            f"Adding subtitiles to clips for video '{video_info.extractor}-{video_info.video_id}'...", total=len(analysis))
+
+        for i, chunk in enumerate(analysis):
+            og_clip_path = os.path.join(
+                video_folder, "clips", f"{i:03d}-{chunk.start}-{chunk.end}.mp4")
+
+            sub_clip_path = os.path.join(
+                clip_folder, f"{i:03d}-{chunk.start}-{chunk.end}-sub.mp4")
+            if os.path.exists(sub_clip_path):
+                continue
+
+            sub_style = "Alignment=10,Fontname=Trebuchet MS,BackColour=&H80000000,Spacing=0.2,Outline=0,Shadow=0.75,PrimaryColour=&H00FFFFFF,Bold=1,MarginV=250,Fontsize=16"
+            # command = f"ffmpeg -y -i \"{og_clip_path}\" -vf 'subtitles=\"{srt_path}\":force_style=\"{sub_style}\"' \"{sub_clip_path}\""
+            command = [
+                "ffmpeg",
+                "-y",
+                "-i",
+                og_clip_path,
+                "-vf",
+                f"subtitles={srt_path}:force_style='{sub_style}'",
+                "-c:a",
+                "copy",
+                sub_clip_path
+            ]
+            output = subprocess.run(command,  capture_output=True)
+            assert output.returncode == 0, f"Failed to add subtitles to clip: {output.stderr.decode('utf-8')}"
             progress.update(main_bar, advance=1)
 
     console.log(
