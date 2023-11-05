b6e43d51c56d637ab4f6bcb80db3b1fe53167e2b
f1shy-dev <56125930+f1shy-dev@users.noreply.github.com>
2023-11-05 17:01:55 +0000

    variable name issue

--- tests/yshort.py
+++ tests/yshort.py
@@ -58,7 +58,7 @@ def gpt_loop(tries = 0):
     prompt_tk = usage_tk['prompt_tokens']
     comp_tk = usage_tk['completion_tokens']
     console.log(
-        f"Used {prompt_tk} prompt + {comp_tk} completion ({usage_tk['total_tokens']} total ~ ${(prompt/1000*0.012) + (comp_tk/1000*0.016)}) tokens.")
+        f"Used {prompt_tk} prompt + {comp_tk} completion ({usage_tk['total_tokens']} total ~ ${(prompt_tk/1000*0.012) + (comp_tk/1000*0.016)}) tokens.")
     content = response["choices"][0]["message"]["content"]
 
     try:
