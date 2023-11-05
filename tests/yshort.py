e3a338ed58a70184abfb36a0f678696e9664bfe9
f1shy-dev <56125930+f1shy-dev@users.noreply.github.com>
2023-11-05 17:21:18 +0000

    stdout...

--- tests/yshort.py
+++ tests/yshort.py
@@ -37,8 +37,8 @@ with console.status("Collating background videos...") as s:
             s.update(f"Collating background videos... (processing #{idx}/{len(videos)} - at {duration}s duration)")
             if video.startswith('rand-'):
                 duration_cmd = f'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 {os.path.join(BACKGROUND_DIR, video)}'
-                duration_output = subprocess.check_output(duration_cmd, shell=True)
-                vid_duration = float(duration_output)
+                duration_output = subprocess.run(duration_cmd.split(), capture_output=True)
+                vid_duration = float(duration_output.stdout)
                 duration += vid_duration
                 start_time = random.uniform(0, duration - 10)
                 output_cmd = f'ffmpeg -y -ss {start_time} -i {os.path.join(BACKGROUND_DIR, video)} -t 10 -c copy workspace/temp/bg-{idx}.mp4'
@@ -46,8 +46,8 @@ with console.status("Collating background videos...") as s:
 
             elif video.startswith('whole-'):
                 duration_cmd = f'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 {os.path.join(BACKGROUND_DIR, video)}'
-                duration_output = subprocess.check_output(duration_cmd, shell=True)
-                vid_duration = float(duration_output)
+                duration_output = subprocess.run(duration_cmd.split(), capture_output=True)
+                vid_duration = float(duration_output.stdout)
                 vid_duration = min(duration, 10)
                 duration += vid_duration
                 output_cmd = f'ffmpeg -y -i {os.path.join(BACKGROUND_DIR, video)} -t {duration} -c copy workspace/temp/bg-{idx}.mp4'
@@ -58,7 +58,7 @@ with console.status("Collating background videos...") as s:
         packlist_file.close()
     s.update("Merging background videos...")
     merge_cmd = 'ffmpeg -f concat -safe 0 -i ffmpeg-packlist-bg.txt -c copy workspace/temp/bg-merge.mp4'
-    subprocess.call(merge_cmd, shell=True, capture_output=True)
+    subprocess.run(merge_cmd.split(), capture_output=True)
 
 # prompt = """you are generating a script for a social media short/reel about facts.
 # the topic for the facts is just "random/interesting facts".
