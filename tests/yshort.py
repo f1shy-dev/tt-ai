409f22490a06cc0c70b4ef2ad3753e3f5344b36e
f1shy-dev <56125930+f1shy-dev@users.noreply.github.com>
2023-11-09 23:32:42 +0000

    convenience

--- tests/yshort.py
+++ tests/yshort.py
@@ -13,8 +13,6 @@ import random
 import torch
 console = Console()
 client = OpenAI()
-# console.log(f"[grey46]Loaded OpenAI API Key: {openai.api_key[:8]}")
-
 
 #fmt: off
 BG_CLIP = './bg-sand.mp4'
@@ -22,8 +20,7 @@ DEVICE = "cuda"
 BATCH_SIZE = 1  # reduce if low on GPU mem
 COMPUTE_TYPE = "float16"  # float16 if using gpu
 MODEL_NAME = 'medium' # jonatasgrosman/wav2vec2-large-xlsr-53-english
-FT_MODEL = "ft:gpt-3.5-turbo-1106:personal:farm-chan:8IVnpqJi"
-#FT_MODEL = "gpt-3.5-turbo-1106"
+FT_MODEL = "ft:gpt-3.5-turbo-1106:personal:farm-chan:8IVnpqJi" # gpt-3.5-turbo-1106
 ALIGN_MODEL = "WAV2VEC2_ASR_BASE_960H"
 MAX_WORDS_PER_SEG = 2
 BACKGROUND_DIR = './workspace/bg-vids'
@@ -31,10 +28,10 @@ OUT_DIR = './workspace/clips/yshort'
 TEMP_DIR = './workspace/temp'
 SAVE_ANALYSIS_DIR = './workspace/temp/saves'
 WATERMARK_IMG = "watermarks/f-plug-wm.png"
+PROMPT = "@facts.r9.txt"
 #fmt: on
 
 [os.makedirs(x, exist_ok=True) for x in [TEMP_DIR, SAVE_ANALYSIS_DIR, OUT_DIR]]
-
 
 if not torch.cuda.is_available():
     BATCH_SIZE = 1
@@ -42,6 +39,92 @@ if not torch.cuda.is_available():
     DEVICE = 'cpu'
 
 console.log("[grey46]Done initalising...")
+
+common_topics = [
+    ["random facts", ["random/interesting facts/curiosities"]],
+    ['love/relationships', ['signs she likes you',
+                            'signs he likes you', 'signs they hate you']],
+    ['girls/boys', ['things girls dont want you to know', 'things boys dont want you to know',
+                    'things girls should know about boys', 'the girls want you to know that']],
+    ['save your life', ['save your life']],
+]
+
+console.print("")
+for idx, tpk in enumerate(common_topics):
+    console.print(f"[light_steel_blue](#{idx}) {tpk[0]}")
+console.print(f"[light_steel_blue](#666) load last analysis from file")
+console.print(f"[light_steel_blue](#999) custom topic")
+
+topic_idx = int(console.input("[medium_purple3]Enter topic: "))
+topic = ''
+if topic_idx == 999:
+    topic = console.input("[medium_purple3]Enter topic title: ")
+elif topic_idx != 666:
+    topic = random.choice(common_topics[topic_idx][1])
+console.log(f"[grey46]Generating script w/ model {FT_MODEL}...")
+
+
+def gpt_loop(tries=0):
+    response = client.chat.completions.create(
+        model=FT_MODEL,
+        messages=[{"role": "system", "content": prompt}],
+        temperature=0.7,
+        max_tokens=512,
+        frequency_penalty=0.07,
+        presence_penalty=0.07,
+        response_format={"type": "json_object"}
+    )
+    prompt_tk = int(response.usage.prompt_tokens)
+    comp_tk = int(response.usage.completion_tokens)
+    console.log(
+        f"Used {prompt_tk} prompt + {comp_tk} completion ({response.usage.total_tokens} total ~ ${(prompt_tk/1000*0.003) + (comp_tk/1000*0.006)}) tokens.")
+    content = response.choices[0].message.content
+
+    try:
+        data = json.loads(content)
+        print(content, file=open(
+            f'{SAVE_ANALYSIS_DIR}/analysis-{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.json', 'w'))
+        return data
+    except Exception as e:
+        if tries > 3:
+            raise ValueError('Content generated is not valid json')
+        else:
+            console.log(
+                f'[red]Content generated is not valid json, trying again ({tries}/3)...')
+            return gpt_loop(tries + 1)
+
+
+if PROMPT.startswith("@"):
+    file_path = os.path.join(os.path.dirname(
+        __file__), f"../ttai_farm/analysis/prompts/{PROMPT[1:]}")
+    prompt = open(file_path, "r").read()
+elif PROMPT.startswith("#"):
+    file_path = PROMPT[1:]
+    prompt = open(file_path, "r").read()
+else:
+    prompt = PROMPT
+
+prompt = prompt.replace('{_{0}_}', topic)
+
+if topic_idx == 666:
+    if os.system("command -v fzf >/dev/null 2>&1") == 0:
+        file_path = os.popen(
+            f"find {SAVE_ANALYSIS_DIR} -type f | fzf").read().strip()
+    else:
+        file_path = input("Enter file name: ")
+    data = json.load(open(file_path, 'r'))
+else:
+    data = gpt_loop()
+joined = ''
+for idx, line in enumerate(data['content']):
+    if line['text'].strip() == '':
+        continue
+    color = 'red' if line['type'] == 'hook' else 'medium_purple3'
+    joined += f'[{color}]{line["text"]}[/{color}]\n'
+
+console.print(joined)
+assert Confirm.ask('Is this script good?')
+
 
 with console.status("Collating background videos...") as s:
     packlist = []
@@ -93,107 +176,6 @@ with console.status("Collating background videos...") as s:
     ffresult = subprocess.run(merge_cmd, capture_output=True)
     assert ffresult.returncode == 0, f"ffmpeg failed: {ffresult.stderr}\n\n$> {' '.join(merge_cmd)}"
     console.log("Generated background video...")
-
-
-common_topics = [
-    ["random facts", ["random/interesting facts/curiosities"]],
-    ['love/relationships', ['signs she likes you',
-                            'signs he likes you', 'signs they hate you']],
-    ['girls/boys', ['things girls dont want you to know', 'things boys dont want you to know',
-                    'things girls should know about boys', 'the girls want you to know that']],
-    ['save your life', ['save your life']],
-]
-
-console.print("")
-for idx, tpk in enumerate(common_topics):
-    console.print(f"[light_steel_blue](#{idx}) {tpk[0]}")
-console.print(f"[light_steel_blue](#666) load last analysis from file")
-console.print(f"[light_steel_blue](#999) custom topic")
-
-topic_idx = int(console.input("[medium_purple3]Enter topic: "))
-topic = ''
-if topic_idx == 999:
-    topic = console.input("[medium_purple3]Enter topic title: ")
-elif topic_idx != 666:
-    topic = random.choice(common_topics[topic_idx][1])
-console.log(f"[grey46]Generating script w/ model {FT_MODEL}...")
-
-prompt = """you are generating a script for a social media short/reel about facts.
-the topic for the facts is just "{_{0}_}".
-make sure to include:
-    * hooks to social media features like "like and follow for more facts" or "comment your favorite fact below"
-    * end the video with either:
-        * a hook like "and so" then start the video with "here are ..." since the video loops, so it will seem like it's a never ending list of facts to increase watch time
-        * something like "follow since you'll never see me again" and a cliffhangery fact/statement
-    * in total, around 10 facts+hooks - minimum 2 hooks
-    * the title of the video - with emojis, ellipses, question marks, exclamation marks, hashtags, etc
-    * facts that would be seen as 'outrageous'/'disturbing' - grabbing the audience's attention - something bizzare or really random if needed, depending on the topic
-
-format in JSON like so:
-{
-    "title": "<title>",
-    "content": [
-        {"text": "<fact>", "type": "fact"},
-        {"text": "<fact>", "type": "fact"},
-        {"text": "<hook>", "type": "hook"},
-        {"text": "<fact>", "type": "fact"},
-        {"text": "<hook>", "type": "hook"},
-        //... and so on
-    ]
-}"""
-prompt = prompt.replace('{_{0}_}', topic)
-
-def gpt_loop(tries=0):
-    response = client.chat.completions.create(
-        model=FT_MODEL,
-        messages=[{"role": "system", "content": prompt}],
-        temperature=0.7,
-        max_tokens=512,
-        frequency_penalty=0.07,
-        presence_penalty=0.07,
-        response_format={"type": "json_object"}
-    )
-    prompt_tk = int(response.usage.prompt_tokens)
-    comp_tk = int(response.usage.completion_tokens)
-    console.log(
-        f"Used {prompt_tk} prompt + {comp_tk} completion ({response.usage.total_tokens} total ~ ${(prompt_tk/1000*0.003) + (comp_tk/1000*0.006)}) tokens.")
-    content = response.choices[0].message.content
-
-    try:
-        data = json.loads(content)
-        print(content, file=open(f'{SAVE_ANALYSIS_DIR}/analysis-{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.json', 'w'))
-        # if not 'content' in data or not 'title' in data:
-        #     raise ValueError('Content generated is not valid json')
-        return data
-    except Exception as e:
-        if tries > 3:
-            raise ValueError('Content generated is not valid json')
-        else:
-            console.log(
-                f'[red]Content generated is not valid json, trying again ({tries}/3)...')
-            return gpt_loop(tries + 1)
-
-
-# data = gpt_loop()
-# data = json.load(open('workspace/temp/data.json', 'r'))
-if topic_idx == 666:
-    # data = json.load(open('workspace/temp/data.json', 'r'))
-    if os.system("command -v fzf >/dev/null 2>&1") == 0:
-        file_path = os.popen(f"find {SAVE_ANALYSIS_DIR} -type f | fzf").read().strip()
-    else:
-        file_path = input("Enter file name: ")
-else:
-    data = gpt_loop()
-joined = ''
-for idx, line in enumerate(data['content']):
-    if line['text'].strip() == '':
-        continue
-    color = 'red' if line['type'] == 'hook' else 'medium_purple3'
-    joined += f'[{color}]{line["text"]}[/{color}]\n'
-
-console.print(joined)
-assert Confirm.ask('Is this script good?')
-
 
 console.log(
     f"[grey46]Loading models whisperx:{MODEL_NAME}, align:{ALIGN_MODEL}")
